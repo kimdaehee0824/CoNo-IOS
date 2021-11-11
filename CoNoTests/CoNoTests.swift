7a9d921648e06a866ebe058b242769e807b45044
김대희 <0824dh@naver.com>
2021-11-11 19:11:32 +0900

    [Setting] 기본 설정

---
+++ CoNoTests/CoNoTests.swift
@@ -0,0 +1,33 @@
+//
+//  CoNoTests.swift
+//  CoNoTests
+//
+//  Created by 김대희 on 2021/11/11.
+//
+
+import XCTest
+@testable import CoNo
+
+class CoNoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
