5bd41f1f73680634492882587b558e1f101f6afe
김대희 <0824dh@naver.com>
2021-11-17 18:45:29 +0900

    [Modify] Widget 해보는데 너무 어려워..... 일단은 중단

--- CovidDeshBoard/CovidDeshBoard.swift
+++ CovidDeshBoard/CovidDeshBoard.swift
@@ -10,25 +10,31 @@ import SwiftUI
 
 struct Provider: TimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date())
+        SimpleEntry(date: Date(), decideCount: String())
     }
-
+    
     func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date())
+        let entry = SimpleEntry(date: Date(), decideCount: String())
         completion(entry)
     }
-
+    
     func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
         var entries: [SimpleEntry] = []
-
+        
         // Generate a timeline consisting of five entries an hour apart, starting from the current date.
         let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate)
-            entries.append(entry)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.4) {
+
         }
 
+        for hourOffset in 0 ..< 5 {
+//            dateGet()
+            let DataString = "\(String(describing: CovidData.data.requstSecideCount()))명"
+            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+            let entry = SimpleEntry(date: entryDate, decideCount: DataString)
+            entries.append(entry)
+        }
+        
         let timeline = Timeline(entries: entries, policy: .atEnd)
         completion(timeline)
     }
@@ -36,32 +42,39 @@ struct Provider: TimelineProvider {
 
 struct SimpleEntry: TimelineEntry {
     let date: Date
+    let  decideCount : String
+    
 }
 
 struct CovidDeshBoardEntryView : View {
     var entry: Provider.Entry
-
+    
     var body: some View {
-        Text(entry.date, style: .time)
+        
+        VStack {
+            Text(entry.decideCount)
+            Text(entry.date, style: .time)
+        }
     }
 }
 
 @main
 struct CovidDeshBoard: Widget {
     let kind: String = "CovidDeshBoard"
-
+    
     var body: some WidgetConfiguration {
         StaticConfiguration(kind: kind, provider: Provider()) { entry in
             CovidDeshBoardEntryView(entry: entry)
         }
-        .configurationDisplayName("My Widget")
-        .description("This is an example widget.")
+        .configurationDisplayName("코로나 상황판")
+        .description("오늘의 코로나 상황을 알려주는 위젯입니다.")
+        .supportedFamilies([.systemMedium])
     }
 }
 
 struct CovidDeshBoard_Previews: PreviewProvider {
     static var previews: some View {
-        CovidDeshBoardEntryView(entry: SimpleEntry(date: Date()))
+        CovidDeshBoardEntryView(entry: SimpleEntry(date: Date(), decideCount: String()))
             .previewContext(WidgetPreviewContext(family: .systemSmall))
     }
 }
